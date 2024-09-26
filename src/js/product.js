202ed090a452716effaa0079d94d0254e711b953
DanielBSellers <sel21002@byui.edu>
2024-09-25 22:01:34 -0600

    2 hours later

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,21 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from './ProductDetails.mjs';
+import { getParams } from './utils.mjs';
 
+
+
+console.log(dataSource.findProductById(productId));
+
+const productId = getParams('product');
 const dataSource = new ProductData("tents");
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const product = urlParams.get('product')
+const product2 = new ProductDetails(productId, dataSource);
+product.init();
+
+
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
@@ -16,3 +30,12 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+  async init() {
+    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    // once we have the product details we can render out the HTML
+    // once the HTML is rendered we can add a listener to Add to Cart button
+    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
+    document.getElementById('addToCart')
+      .addEventListener('click', this.addToCart.bind(this));
+    }
