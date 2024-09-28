4a663d450b9424d539e7080f08d3f766255b7827
Sport97 <por21022@byui.edu>
2024-09-28 15:34:11 -0500

    finished week 02 team activity

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,94 +1,59 @@
-import { setLocalStorage } from "./utils.mjs";
-
-import { getLocalStorage } from "./utils.mjs";
- 
-function productDetailsTemplate(product) {
-
-    return `<section class="product-detail">
-<h3>${product.Brand.Name}</h3>
-<h2 class="divider">${product.NameWithoutBrand}</h2>
-<img
-
-          class="divider"
-
-          src="${product.Image}"
-
-          alt="Image of ${product.NameWithoutBrand}"
-
-        />
-<p class="product-card__price">$${product.FinalPrice}</p>
- 
-        <p class="product__color">${product.Colors[0].ColorName}</p>
- 
-        <p class="product__description">
-
-          ${product.DescriptionHtmlSimple}
-</p>
- 
-        <div class="product-detail__add">
-<button id="addToCart" data-id="${product.Id}">Add to Cart</button>
-</div>
-</section>`;
-
-}
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
  
 export default class ProductDetails {
-
     constructor(productId, dataSource){
-
         this.productId = productId;
-
         this.product = {};
-
         this.dataSource = dataSource;
-
       }    
  
       async init() {
-
-        // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-
         this.product = await this.dataSource.findProductById(this.productId);
-
-        this.renderProductDetails("main");
-
-        // once we have the product details we can render out the HTML
-
-        // once the HTML is rendered we can add a listener to Add to Cart button
-
-        // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-
-        document
-
-          .getElementById("addToCart")
-
-          .addEventListener("click", this.addProductToCart.bind(this));
-
+        console.log("Fetched product details:", this.product);
+        this.renderProductDetails();
+        const addToCartButton = document.getElementById('addToCart');
+        if (addToCartButton) {
+          addToCartButton.addEventListener('click', this.addToCart.bind(this));
+        } else {
+          console.error("Add to Cart button not found!");
+        }
+      }
+ 
+      renderProductDetails() {
+        const productContainer = document.getElementById('dynamic-product-details');
+    
+        if (this.product) {
+          productContainer.innerHTML = `<section class="product-detail"> <h3>${this.product.Brand.Name}</h3>
+          <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+          <img
+            class="divider"
+            src="${this.product.Image}"
+            alt="${this.product.NameWithoutBrand}"
+          />
+          <p class="product-card__price">$${this.product.FinalPrice}</p>
+          <p class="product__color">${this.product.Colors[0].ColorName}</p>
+          <p class="product__description">
+          ${this.product.DescriptionHtmlSimple}
+          </p>
+          <div class="product-detail__add">
+            <button id="addToCart" data-id="${this.product.Id}">Add to Cart</button>
+          </div></section>`;
+        } else {
+          productContainer.innerHTML = '<p>Product not found</p>';
+        }
       }
 
-      addProductToCart() {
-
-        const existingCart = getLocalStorage("addToCart") || []; // Retrieve existing items
-
-        existingCart.push(this.product); // Append the new product
-
-        setLocalStorage("addToCart", existingCart); // Save back to local storage
- 
-    }
- 
-      renderProductDetails(selector) {
- 
-        const element = document.querySelector(selector);
-
-        element.insertAdjacentHTML(
-
-          "afterBegin",
-
-          productDetailsTemplate(this.product)
-
-        );
-
+      addToCart() {
+        let cart = getLocalStorage("shoppingCart");
+        const productExists = cart.some((item) => item.Id === this.product.Id);
+    
+        if (productExists) {
+            alert(`${this.product.Name} is already in your cart.`);
+        } else {
+            cart.push(this.product);
+            setLocalStorage("shoppingCart", cart);
+            alert(`${this.product.Name} has been added to your cart!`);
+        }
       }
-
 }
  
--- src/js/product.js
+++ src/js/product.js
@@ -1,39 +1,8 @@
-import { setLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ProductData from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
-import { getParams } from './utils.mjs';
+import { getParam } from './utils.mjs';
 
-
-
-console.log(dataSource.findProductById(productId));
-
-const productId = getParams('product');
-const dataSource = new ProductData("tents");
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const product = urlParams.get('product')
-const product2 = new ProductDetails(productId, dataSource);
+const productId = getParam('product');
+const dataSource = new ProductData('tents');
+const product = new ProductDetails(productId, dataSource);
 product.init();
-
-function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-
-  async init() {
-    // use our datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
-    // once we have the product details we can render out the HTML
-    // once the HTML is rendered we can add a listener to Add to Cart button
-    // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-    document.getElementById('addToCart')
-      .addEventListener('click', this.addToCart.bind(this));
-    }
