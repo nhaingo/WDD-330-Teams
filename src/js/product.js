37c60d3030fe24769fcc90ac65454ef6cf30823b
Sport97 <por21022@byui.edu>
2024-10-04 19:49:52 -0500

    expand inventory, week03 individual activity

--- src/js/product.js
+++ src/js/product.js
@@ -2,9 +2,8 @@ import ProductData from './ProductData.mjs';
 import ProductDetails from './ProductDetails.mjs';
 import { getParam, loadHeaderFooter } from './utils.mjs';
 
-const productId = getParam('product');
 const dataSource = new ProductData('tents');
+const productId = getParam('product');
 const product = new ProductDetails(productId, dataSource);
-
 product.init();
 loadHeaderFooter();
